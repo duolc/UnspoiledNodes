b8b5d9f0ca5bf713b7b775c876f25eaaab181017
Duolc <ryan.johnson.j@gmail.com>
2015-04-16 23:22:35 -0400

    Revert "Noting Of Value Accopmplished"
    This reverts commit 4154ca76c2e3a37b3c57eac5ab1d56e9d9651308.

--- STAGE/functions.js
+++ STAGE/functions.js
@@ -241,12 +241,3 @@ $('#Windy').change(function () {
   if (!this.checked) $('.Windy').fadeOut('slow');
     else $('.Windy').fadeIn('slow');
 });
-
-var tooltipSpan = document.getElementById('tooltip-span');
-
-window.onmousemove = function (e) {
-    var x = e.clientX,
-        y = e.clientY;
-    tooltipSpan.style.top = (y + 20) + 'px';
-    tooltipSpan.style.left = (x + 20) + 'px';
-};
