4154ca76c2e3a37b3c57eac5ab1d56e9d9651308
Duolc <ryan.johnson.j@gmail.com>
2015-04-16 23:22:30 -0400

    Noting Of Value Accopmplished
    :(

--- STAGE/functions.js
+++ STAGE/functions.js
@@ -241,3 +241,12 @@ $('#Windy').change(function () {
   if (!this.checked) $('.Windy').fadeOut('slow');
     else $('.Windy').fadeIn('slow');
 });
+
+var tooltipSpan = document.getElementById('tooltip-span');
+
+window.onmousemove = function (e) {
+    var x = e.clientX,
+        y = e.clientY;
+    tooltipSpan.style.top = (y + 20) + 'px';
+    tooltipSpan.style.left = (x + 20) + 'px';
+};
